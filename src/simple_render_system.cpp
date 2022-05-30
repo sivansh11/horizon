59f19be03f05c3d63f8cc62807dd306cb4bb1c09
sivansh11 <sivanshgupta2002@gmail.com>
2022-05-30 22:56:37 +0530

    weird debug mode only bug

--- src/simple_render_system.cpp
+++ src/simple_render_system.cpp
@@ -61,6 +61,7 @@ void SimpleRenderSystem::renderGameObjects(VkCommandBuffer commandBuffer, std::v
     for (auto& obj: gameObjects)
     {
         obj.transform.rotation.y = glm::mod(obj.transform.rotation.y + 0.01f, glm::two_pi<float>());
+        obj.transform.rotation.x = glm::mod(obj.transform.rotation.x + 0.01f, glm::two_pi<float>());
 
         SimplePushConstantData push{};
         push.color = obj.color;
