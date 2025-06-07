2a07ba242d2f31e11f36f070b6d0f21c4180b61c
sivansh11 <sivanshgupta2002@gmail.com>
2025-06-07 10:52:09 +0530

    force flush logger after ever log (maybe make this optional ?)

--- src/core/logger.cpp
+++ src/core/logger.cpp
@@ -37,24 +37,28 @@ void log_t::set_error_color(const std::string &str) { _log_colors[3] = str; }
 void log_t::log_trace(const std::string &str) {
   if (_log_level <= log_level_t::e_trace) {
     (*log_stream) << _log_colors[0] << str << "\e[0;97m" << '\n';
+    log_stream->flush();
   }
 }
 
 void log_t::log_info(const std::string &str) {
   if (_log_level <= log_level_t::e_info) {
     (*log_stream) << _log_colors[1] << str << "\e[0;97m" << '\n';
+    log_stream->flush();
   }
 }
 
 void log_t::log_warn(const std::string &str) {
   if (_log_level <= log_level_t::e_warn) {
     (*log_stream) << _log_colors[2] << str << "\e[0;97m" << '\n';
+    log_stream->flush();
   }
 }
 
 void log_t::log_error(const std::string &str) {
   if (_log_level <= log_level_t::e_error) {
     (*log_stream) << _log_colors[3] << str << "\e[0;97m" << '\n';
+    log_stream->flush();
   }
 }
 
